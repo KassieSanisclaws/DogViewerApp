af74d7b1e3406465f3fb2756ff0c59a940babb34
Kadeem Cherman <Kassie35dev@gmail.com>
2026-06-16 16:22:51 -0600

    Initial Commit

---
+++ DogViewer/DogViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogViewerApp.swift
+//  DogViewer
+//
+//  Created by Kadeem Cherman on 2026-06-16.
+//
+
+import SwiftUI
+
+@main
+struct DogViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
